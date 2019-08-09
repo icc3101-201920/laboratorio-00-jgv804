7ec68befeb9846f17d146ccc46a6d8225b998208
juan guillermo <jvergara5@miuandes.cl>
2019-08-09 09:45:16 -0400

    first commit

--- ConsoleApp1/ConsoleApp1/Class2.cs
+++ ConsoleApp1/ConsoleApp1/Class2.cs
@@ -8,17 +8,52 @@ namespace ConsoleApp1
 {
     public class CombatCard
     {
-        private string name;
-        private int attackPoints;
-        private bool hero;
-        private string type;
-        private string effect;
+        private string Name;
+        private int AttackPoints;
+        private bool Hero;
+        private string Type;
+        private string Effect;
 
         public string getname()
         {
-            return name;
+            return Name;
         }
-
+        public void setname(string name)
+        {
+            name = Name;
+        }
+        public int getAttackPoints()
+        {
+            return AttackPoints;
+        }
+        public void setAttackPoints(int attackpoints)
+        {
+            attackpoints = attackPoints;
+        }
+        public bool gethero()
+        {
+            return hero;
+        }
+        public void setname(bool Hero)
+        {
+            Hero = hero;
+        }
+        public string gettype()
+        {
+            return type;
+        }
+        public void settype(string type)
+        {
+            Hero = hero;
+        }
+        public bool gethero()
+        {
+            return hero;
+        }
+        public void setname(bool Hero)
+        {
+            Hero = hero;
+        }
 
     }
 }
