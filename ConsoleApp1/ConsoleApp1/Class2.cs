a40ab9caa3c4eea8b732906bebe1e335912c9c25
juan guillermo <jvergara5@miuandes.cl>
2019-08-09 09:42:29 -0400

    first commit

--- ConsoleApp1/ConsoleApp1/Class2.cs
+++ ConsoleApp1/ConsoleApp1/Class2.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Class2
+    public class CombatCard
     {
+        private string name;
+        private int attackPoints;
+        private bool hero;
+        private string type;
+        private string effect;
+
+        public string getname()
+        {
+            return name;
+        }
+
+
     }
 }
